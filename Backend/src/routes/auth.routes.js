6519908ddf402149d0683511a60458f6a3677cb5
Harshit Chauhan <harshit.honey003@gamil.com>
2026-04-10 17:28:50 +0530

    created login api

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -52,4 +52,48 @@ authRouter.post("/register", async (req, res) => {
   });
 });
 
+authRouter.post("/login", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const user = await userModel.findOne({
+    $or: [
+      {
+        username: username,
+      },
+      {
+        email: email,
+      },
+    ],
+  });
+
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found",
+    });
+  }
+
+  const hash = crypto.createHash("sha256").update(password).digest("hex");
+  const isPasswordValid = hash == user.password;
+
+  if (!isPasswordValid) {
+    return res.status(401).json({
+      message: "password invalid",
+    });
+  }
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+  res.cookie("token", token);
+  res.status(200).json({
+    message: "User loggedIn successfully",
+    user: {
+      username: user.username,
+      email: user.email,
+      Bio: user.Bio,
+      profileImage: user.profileImage,
+    },
+  });
+});
+
 module.exports = authRouter;
